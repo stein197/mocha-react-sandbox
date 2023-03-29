56bf7fc59860e4beea12bf39ddb9b8b6dac4f8ee
Nail Gafarov <nil20122013@gmail.com>
2023-03-29 13:30:04 +0500

    Updated tests

--- test.js
+++ test.js
@@ -2,16 +2,20 @@ const React = require("react");
 const sandbox = require(".");
 
 function Component1() {
-	return React.createElement("div", null, "Hello, World!");
+	const [state, setState] = React.useState(0);
+	return React.createElement(React.Fragment, null,
+		React.createElement("p", null, state),
+		React.createElement("button", {onClick: () => setState(prev => ++prev)}, "Click")
+	);
 }
 
 sandbox(globalThis, sb => {
 	it("assert()", () => sb.assert(sb => "String", "String").run());
 	it.skip("await()", () => {});
-	it.skip("find()", () => {});
-	it.skip("findByText()", () => {});
-	it("render()", () => sb.render(React.createElement(Component1)).assert(sb => sb.innerHTML, "<div>Hello, World!</div>").run());
-	it.skip("simulate()", () => {});
+	it("find()", () => sb.render(React.createElement(Component1)).assert(sb => sb.find("p").textContent, "0").run());
+	it("findByText()", () => sb.render(React.createElement(Component1)).assert(sb => sb.findByText("Click").textContent, "Click").run());
+	it("render()", () => sb.render(React.createElement(Component1)).assert(sb => sb.innerHTML, "<p>0</p><button>Click</button>").run());
+	it("simulate()", () => sb.render(React.createElement(Component1)).simulate(sb => sb.find("button"), "click").assert(sb => sb.find("p").textContent, "1"));
 	it.skip("timeout()", () => {});
 	it("Complex behavior", () => {});
 });
