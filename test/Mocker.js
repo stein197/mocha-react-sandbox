4012891ede6759b826e5b7bf9b2999ab66004e05
Nail Gafarov <nil20122013@gmail.com>
2023-05-03 12:57:25 +0500

    Added test stubs for Mocker

---
+++ test/Mocker.js
@@ -0,0 +1,13 @@
+const Mocker = require("../src/Mocker");
+
+// TODO
+describe("Mocker", () => {
+	// TODO
+	describe("Mocker.mock()", () => {});
+	// TODO
+	describe("Mocker.unmock()", () => {});
+	// TODO
+	describe("Mocker.getOriginal()", () => {});
+	// TODO
+	describe("Mocker.clean()", () => {});
+});
